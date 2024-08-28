7dd4db4291e31cf411bb13cb1bf2f12993c803e3
Elena Capatana <helly.ec@gmail.com>
2024-08-28 21:17:46 +0200

    add logo and third png

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -18,9 +18,26 @@ const Home = (): JSX.Element => {
         className="absolute top-6 right-3 -rotate-[295deg] drop-shadow-[2px_-2px_4px_rgba(0,0,0,0.25)]"
         //      className="absolute top-6 right-3 -rotate-[295deg] drop-shadow-custom" still testing
       />
+      <div class="relative flex items-center justify-center h-screen">
+        <Image
+          src="/fao-web-assets-logo-vertical-colour.svg"
+          alt="Anna und Otto - Das Familiencafe Logo"
+          width={269}
+          height={275}
+          className="absolute z-20 bg-[rgba(255,255,255,0.75)]"
+        />
+      </div>
+      <Image
+        src="/images/coffee-beans.png"
+        alt="Haufen von Kaffeebohnen"
+        width={386}
+        height={386}
+        className="absolute bottom-6 -right-28"
+      />
     </>
   );
 };
 export default Home;
 //2px_-2px_4px_rgba(0,0,0,0.25)
 //0_1px_2px_rgba(0,0,0,0.44)
+//drop-shadow-[5px_-5px_5px_rgb(255,255,255)]
