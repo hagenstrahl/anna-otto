fc328fbbc6c337cc95789745475c218b38dc9011
ELENA_CAPATANA <helly.ec@gmail.com>
2025-06-16 18:14:51 +0200

    create branch for add menu feature (#26)

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Menu } from "iconoir-react";
 import { Metadata } from "next";
 import DesktopFooter from "../components/desktop-footer";
 import MobileStickyFooter from "../components/mobile-footer";
@@ -14,6 +15,9 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body>
+        <button className="fixed top-[50px] right-4 w-10 h-10 flex item-center justify-center z-50">
+          <Menu className="w-6 h-6 " />
+        </button>
         {children}
         <MobileStickyFooter />
         <DesktopFooter />
