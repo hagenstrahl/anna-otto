b360ee204dfb979bff38dba072b178247cb6df24
ELENA_CAPATANA <helly.ec@gmail.com>
2025-07-07 18:41:26 +0200

    add framer-motion animation

--- apps/website/components/mobile-menu.tsx
+++ apps/website/components/mobile-menu.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { AnimatePresence, motion } from "framer-motion";
 import { Menu, Xmark } from "iconoir-react";
 import { useState } from "react";
 
@@ -26,61 +26,74 @@ export default function MobileMenu() {
           <Menu className="w-6 h-6 text-orange" strokeWidth={2.5} />
         )}
       </button>
-      {isMenuOpen && (
-        <div className="fixed inset-0 z-40 bg-white md:hidden">
-          <div className="flex flex-col items-center justify-center h-full">
-            <img
-              src="/fao-web-assets-logo-vertical-colour.svg"
-              alt="Logo"
-              className="absolute"
-              style={{ width: "86.65px", height: "88.86px", top: "4rem" }}
-            />
-            <nav className="flex flex-col space-y-8 text-center">
-              <a
-                href="/"
-                className="text-2xl font-text text-grau"
-                onClick={closeMenu}
-              >
-                Café
-              </a>
-              <a
-                href="/"
-                className="text-2xl font-text text-grau"
-                onClick={closeMenu}
-              >
-                Shop
-              </a>
-              <a
-                href="/"
-                className="text-2xl font-text text-grau"
-                onClick={closeMenu}
-              >
-                Beratung & Kurse
-              </a>
-              <a
-                href="/"
-                className="text-2xl font-text text-grau"
-                onClick={closeMenu}
-              >
-                Kontakt
-              </a>
-            </nav>
-            <div className="flex items-center mt-12 space-x-4 text-lg text-grau font-text">
-              <a href="/impressum" onClick={closeMenu}>
-                Impressum
-              </a>
-              <span>|</span>
-              <a href="/datenschutz" onClick={closeMenu}>
-                Datenschutz
-              </a>
-              <span>|</span>
-              <a href="/" onClick={closeMenu}>
-                AGB
-              </a>
+
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{
+              type: "tween",
+              ease: "easeInOut",
+              duration: 0.5,
+            }}
+            className="fixed inset-0 z-40 bg-white md:hidden"
+          >
+            <div className="flex flex-col items-center justify-center h-full">
+              <img
+                src="/fao-web-assets-logo-vertical-colour.svg"
+                alt="Logo"
+                className="absolute"
+                style={{ width: "86.65px", height: "88.86px", top: "4rem" }}
+              />
+              <nav className="flex flex-col space-y-8 text-center">
+                <a
+                  href="/"
+                  className="text-2xl font-text text-grau"
+                  onClick={closeMenu}
+                >
+                  Café
+                </a>
+                <a
+                  href="/"
+                  className="text-2xl font-text text-grau"
+                  onClick={closeMenu}
+                >
+                  Shop
+                </a>
+                <a
+                  href="/"
+                  className="text-2xl font-text text-grau"
+                  onClick={closeMenu}
+                >
+                  Beratung & Kurse
+                </a>
+                <a
+                  href="/"
+                  className="text-2xl font-text text-grau"
+                  onClick={closeMenu}
+                >
+                  Kontakt
+                </a>
+              </nav>
+              <div className="flex items-center mt-12 space-x-4 text-lg text-grau font-text">
+                <a href="/impressum" onClick={closeMenu}>
+                  Impressum
+                </a>
+                <span>|</span>
+                <a href="/datenschutz" onClick={closeMenu}>
+                  Datenschutz
+                </a>
+                <span>|</span>
+                <a href="/" onClick={closeMenu}>
+                  AGB
+                </a>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
