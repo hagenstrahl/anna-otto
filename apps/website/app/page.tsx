0384c29966888cb2f4e2f6596a4d25380651c01d
Hagen Strahl <34143718+hagenstrahl@users.noreply.github.com>
2026-03-11 17:05:25 +0000

    remove part for Beratungsraum

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -2,8 +2,10 @@ import Heading, { HeadingType } from "../components/heading";
 
 const Home = (): JSX.Element => {
   return (
-  <>
-      <Heading type={HeadingType.H1} className="mt-8">Coming Soon!</Heading>
+    <>
+      <Heading type={HeadingType.H1} className="mt-8">
+        Coming Soon!
+      </Heading>
       <section className="max-w-3xl text-center">
         Ob wärmende Strahlen der ersten Frühlingssonne oder bewölkter Himmel mit
         Nieselregen – ab jetzt gibt es mindestens einen guten Grund, auch bei
@@ -14,13 +16,9 @@ const Home = (): JSX.Element => {
         ausgiebiges Faxen machen in unserer großen Spielecke.
       </section>
       <section className="max-w-3xl mt-5 text-center">
-        Übrigens: In einem separaten Beratungs- und Seminarraum erfahrt ihr
-        alles rund um die Themen Schwangerschaft und Familie.
-      </section>
-      <section className="max-w-3xl mt-5 text-center">
         Wir freuen uns auf euch. Bis bald!
       </section>
-      </>
+    </>
   );
 };
 export default Home;
